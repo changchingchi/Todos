875a19f5d51e079695548f03ecb86757a9a9d209
chchi <chi.changching@gmail.com>
2016-10-27 20:52:36 -0700

    add save symbol in editactivity

--- app/src/main/java/com/chchi/todo/EditItemActivity.java
+++ app/src/main/java/com/chchi/todo/EditItemActivity.java
@@ -3,6 +3,7 @@ package com.chchi.todo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class EditItemActivity extends AppCompatActivity {
 
@@ -12,5 +13,12 @@ public class EditItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_item);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edititem, menu);
+        return true;
     }
 }
